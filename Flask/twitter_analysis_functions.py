6dbe4f7990e17b444a5cd4bc80bca09de8b4ed58
luiscabanzon <luiscabanzon@gmail.com>
2015-06-16 20:01:05 +0200

    Analysis automation ongoing. Needs to solve the API calling limit error.

--- Flask/twitter_analysis_functions.py
+++ Flask/twitter_analysis_functions.py
@@ -17,19 +17,71 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-def analysis(trending_topic):
+_pwd_ = "/home/honu/projects/twitter/Flask/"
+
+##################################
+#    Save and load functions     #
+##################################
+def get_trending_topics():
+	trends = api.trends_place(1)
+	trends_list = []
+	today = str(datetime.today())[:10]
+	for i in trends[0]["trends"]:
+		trend =  str(i["name"].encode("utf-8"))
+		trends_list.append(trend)
+	with open(_pwd_ + "db/trends/" + today + "_trends.txt", 'wb') as f:
+		pickle.dump(trends_list, f)
+
+def load_trending_topics(date):
+	with open(_pwd_ + "db/trends/" + date + "_trends.txt", 'rb') as f:
+		trends = pickle.load(f)
+	return trends
+
+
+def load_data(date, trending_topic):
+	with open(_pwd_ + "db/raw_data/" + date + "_" +  trending_topic, 'rb') as f:
+		data = pickle.load(f)
+	return data
+
+##########################
+# Analysis Main Function #
+##########################
+def download_tweets(trending_topic):
 	last_month = str(datetime.today() - timedelta(days=30))[:10]
 	today = str(datetime.today())[:10]
 	tomorrow = str(datetime.today() + timedelta(days=1))[:10]
 	trend = tweepy.Cursor(api.search, q=trending_topic, since=last_month, until=tomorrow).items()
 	data = []
+	counter = 0
 	for i in trend:
+		counter = counter + 1
 		try:
 			data.append(i)
-			data_text = ' '.join([data[i].text.lower() for i in xrange(len(data))]).encode('utf-8')
+		except tweepy.TweepError:
+			break
 
-	with open("/home/honu/projects/twitter/Flask/" + "db/data/" + today + "_" + trending_topic + ".txt", 'wb') as f:
+	with open(_pwd_ + "db/raw_data/" + today + "_" + trending_topic + ".txt", 'wb') as f:
 		pickle.dump(data, f)
+
+	return data
+
+def analysis(data, trending_topic):
+	today = str(datetime.today())[:10]
+	data_text = ""
+	for i in data:
+		data_text = ' '.join([data[i].text.lower() for i in xrange(len(data))]).encode('utf-8')
+
+	hashtags = []
+	mentions = []
+	for i in data:
+		for j in i._json["entities"]["hashtags"]:
+			hashtags.append(j["text"].encode('utf-8'))
+		for j in i._json["entities"]["user_mentions"]:
+			mentions.append(j["screen_name"].encode('utf-8'))
+
+	hashtags_frequency = collections.Counter(hashtags)
+	mentions_frequency = collections.Counter(mentions)
+
 
 	df = pd.DataFrame(columns= ["created_at", "user", "favs", "rt", "lang", "time_zone", "text", "user_pic"])
 	for entry in data:
@@ -37,7 +89,7 @@ def analysis(trending_topic):
 
 	output = {}
 	top_hashtags = hashtags_frequency.most_common(10)
-	top_users = at_user_frequency.most_common(10)
+	top_users = mentions_frequency.most_common(10)
 	most_fav = df[df["favs"] == df["favs"].max()].head(1)
 	most_rt = data[df[df["rt"] == df["rt"].max()].head(1).index[0]].retweeted_status
 	time_zones = collections.Counter(df["time_zone"]).most_common(25)
@@ -53,9 +105,10 @@ def analysis(trending_topic):
 
 	json_output = json.dumps(output)
 
-	with open("/home/honu/projects/twitter/Flask/" + "db/json/"today + "_" + trending_topic + "_json.txt", 'wb') as f:
+	with open(_pwd_ + "db/json/" + today + "_" + trending_topic + "_json.txt", 'wb') as f:
 		pickle.dump(json_output, f)
 
+	return json_output
 
 
 ###########################
@@ -81,3 +134,18 @@ def get_time_zone(entry):
 		return "N/A"
 	else:
 		return entry._json["user"]["time_zone"].encode("utf-8")
+
+
+
+def complete_process(trending_topic):
+	analysis(download_tweets(trending_topic))
+
+
+
+
+#################
+
+#with open("/home/honu/projects/twitter/fallout4.txt", 'rb') as f:
+#	test = pickle.load(f)
+
+analysis(download_tweets("#ThreeWordsSheWantsToHear"), trending_topic = "#ThreeWordsSheWantsToHear")
